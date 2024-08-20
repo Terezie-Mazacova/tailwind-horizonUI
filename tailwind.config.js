b55d63459cec4b815cd5117cf019aaa2a771b5db
Terezie Mazáčová <terezkamazacova@post.cz>
2024-08-20 23:54:21 +0200

    zalozeni noveho projektu v TW, z pulky hotovy header a hero sekce

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
