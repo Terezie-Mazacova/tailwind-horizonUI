f7005c16e4a4ab8c9d40b96a6f89678816206f6c
Terezie Mazáčová <terezkamazacova@post.cz>
2024-08-21 02:46:35 +0200

    hotova cela sekce nav a hero, stavy tlacitek

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     fontFamily: {
       jakarta: ['Plus Jakarta Sans', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'primary-purple-blue-100' : '#E9E3FF',
+        'primary-purple-blue-200' : '#B9A2FF',
+        'primary-purple-blue-400' : '#7551FF',
+        'primary-purple-blue-500' : '#4318FF',
+        'primary-purple-blue-600' : '#3311DB',
+
+        'secondary-dark-grey-600' : '#68769F',
+        'secondary-dark-grey-900' : '#1B2559',
+      },
+
+      maxWidth: {
+        '1440' : '1440px',
+      },
+    },
   },
   plugins: [],
 }
